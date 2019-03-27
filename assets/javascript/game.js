dcb9fcf5295e36b8ef7b77373fed346baa317ee4
Kim Gorcyca <kgorcyca@gmail.com>
2019-03-26 18:30:20 -0700

    completed css, updated html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,61 @@
 // set initail variables for wins, losses, remaining guesses, 
 // and previous guesses
-var gameDetails =[
-  letters = ['a','b','c','d','e','f','g','h','i',
-              'j','k','l','m','n','o','p','q','r',
-              's','t','u','v','w','x','y','z'
-              ],
-  winsCounter = 0,
-  lossesCounter = 0,
-  guessesLeft = 10,
-  userGuesses = [], 
- ]
+var computerChoices = ['a','b','c',
+              'd','e','f','g','h','i'];
+              // 'j','k','l','m','n','o','p','q','r',
+              // 's','t','u','v','w','x','y','z'
+              // ];
+var wins = 0;
+var losses = 0;
+var numGuess = 15;
 
-console.log(gameDetails)
+console.log(computerChoices);
 
-// // Create variables that hold references to the places in the HTML where we want to display things.
+// Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var numGuess = document.getElementById("numGuess-text");
+// var prevGuessText = document.getElementById ("prevGuess-text")
+var numGuessText = document.getElementById("numGuess-text");
 
-window.onload = function () {
-  var letterGuess = letters[Math.floor(Math.random() * letters.length)]; 
-  }
 // This function is run whenever the user presses a key.
- document.onkeyup = function(event) {
+document.onkeyup = function(event) {
 
 // Determines which key was pressed.
-  var userGuess = event.key;
+    var userGuess = event.key;
 
-// creating "else if" statements to determine the outcome of game.
+    console.log(userGuess)
 
-   if (userGuesses === letterGuess) {
-        winsCounter ++ ; 
-        guessesLeft -- ;
-        alert ("Wowza, you got " + winsCounter + " right, you must be psychic!")}
+// Randomly chooses a choice from the options array. This is the Computer's choice.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+// creating "if/else" statement to determine the outcome of game.
+
+   if (userGuess === computerGuess) {
+        wins ++ ; 
+        numGuess -- ;
+        alert ("Wowza, you got " + wins + " right, you must be psychic!")}
     else {
-        lossesCounter ++ ;
-        guessesLeft -- ;
-        alert ("Nope, you got " + lossesCounter + " wrong, you do not possess the gift!")
+        losses ++ ;
+        numGuess -- ;
+        alert ("Nope, you got " + losses + " wrong, you do not possess the gift!")
         }
+    
+    if (numGuess === 0) {
+      alert( "Game over... but you knew that")
+    }
+
+// Hide the directions
+    directionsText.textContent = "";
+
+// Display the user and computer guesses, and wins/losses/remaining guesses.
+    userChoiceText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    // prevGuessText.textContent = "Your guesses so far: " + prevGuess;
+    numGuessText.textContent = "Guesses Left: " + numGuess;
   }
